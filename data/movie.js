56e882bf3ba878f6f7d0197e83b431a6f7ca4042
benny <bennydrm16@gmail.com>
2021-06-06 09:37:07 +0700

    add showing today

--- data/movie.js
+++ data/movie.js
@@ -3,185 +3,185 @@ const movies = [
     title: "Loki",
     type: "Series",
     image: "img/loki.jpg",
-    realeaseDate: '9 Jun 2021',
+    realeaseDate: "9 Jun 2021",
     year: 2021,
-    details: "https://filmbenia.vercel.app/tv/84958"
+    details: "https://filmbenia.vercel.app/tv/84958",
   },
   {
     title: "Black Widow",
     type: "Movie",
     image: "img/blackwidow.jpg",
-    realeaseDate: '9 Jul 2021',
+    realeaseDate: "9 Jul 2021",
     year: 2021,
-    details: "https://filmbenia.vercel.app/movie/497698"
+    details: "https://filmbenia.vercel.app/movie/497698",
   },
   {
     title: "Shang Chi and the Legend of the Ten Rings",
     type: "Movie",
     image: "img/shangchi.jpg",
-    realeaseDate: '3 Sep 2021',
+    realeaseDate: "3 Sep 2021",
     year: 2021,
-    details: "https://filmbenia.vercel.app/movie/566525"
+    details: "https://filmbenia.vercel.app/movie/566525",
   },
   {
     title: "Eternals",
     type: "Movie",
     image: "img/eternals.jpg",
-    realeaseDate: '5 Nov 2021',
+    realeaseDate: "5 Nov 2021",
     year: 2021,
-    details: "https://filmbenia.vercel.app/movie/524434"
+    details: "https://filmbenia.vercel.app/movie/524434",
   },
   {
     title: "Spider-Man: No Way Home",
     type: "Movie",
     image: "img/spiderman-no-way-home.jpg",
-    realeaseDate: '17 Dec 2021',
+    realeaseDate: "17 Dec 2021",
     year: 2021,
-    details: "https://filmbenia.vercel.app/movie/634649"
+    details: "https://filmbenia.vercel.app/movie/634649",
   },
   {
     title: "Doctor Strange in the Multiverse of Madness",
     type: "Movie",
     image: "img/drstrange-itmom.jpg",
-    realeaseDate: '25 Mar 2022',
+    realeaseDate: "25 Mar 2022",
     year: 2022,
-    details: "https://filmbenia.vercel.app/movie/453395"
+    details: "https://filmbenia.vercel.app/movie/453395",
   },
   {
     title: "Thor: Love and Thunder",
     type: "Movie",
     image: "img/thor-love-and-thunder.jpg",
-    realeaseDate: '6 May 2022',
+    realeaseDate: "6 May 2022",
     year: 2022,
-    details: "https://filmbenia.vercel.app/movie/616037"
+    details: "https://filmbenia.vercel.app/movie/616037",
   },
   {
     title: "Black Panther: Wakanda Forever",
     type: "Movie",
     image: "img/black-panther2.jpg",
-    realeaseDate: '8 Jul 2022',
+    realeaseDate: "8 Jul 2022",
     year: 2022,
-    details: "https://filmbenia.vercel.app/movie/505642"
+    details: "https://filmbenia.vercel.app/movie/505642",
   },
   {
     title: "What If...?",
     type: "Series",
     image: "img/whatif.jpg",
-    realeaseDate: '',
+    realeaseDate: "",
     year: 2021,
-    details: "https://filmbenia.vercel.app/tv/91363"
+    details: "https://filmbenia.vercel.app/tv/91363",
   },
   {
     title: "Hawkeye",
     type: "Series",
     image: "img/hawkeye.jpg",
-    realeaseDate: '',
+    realeaseDate: "",
     year: 2021,
-    details: "https://filmbenia.vercel.app/tv/88329"
+    details: "https://filmbenia.vercel.app/tv/88329",
   },
   {
     title: "Ms. Marvel",
     type: "Series",
     image: "img/msmarvel.jpg",
-    realeaseDate: '',
+    realeaseDate: "",
     year: 2021,
-    details: "https://filmbenia.vercel.app/tv/92782"
+    details: "https://filmbenia.vercel.app/tv/92782",
   },
   {
     title: "She Hulk",
     type: "Series",
     image: "img/shehulk.jpg",
-    realeaseDate: '',
+    realeaseDate: "",
     year: 2022,
-    details: "https://filmbenia.vercel.app/tv/92783"
+    details: "https://filmbenia.vercel.app/tv/92783",
   },
   {
     title: "The Marvels",
     type: "Movie",
     image: "img/captain-marvel2.jpg",
-    realeaseDate: '11 Nov 2022',
+    realeaseDate: "11 Nov 2022",
     year: 2022,
-    details: "https://filmbenia.vercel.app/movie/609681"
+    details: "https://filmbenia.vercel.app/movie/609681",
   },
   {
     title: "Ant-Man and the Wasp: Quantumania",
     type: "Movie",
     image: "img/antman3.jpg",
-    realeaseDate: '17 Feb 2023',
+    realeaseDate: "17 Feb 2023",
     year: 2023,
-    details: "https://filmbenia.vercel.app/movie/640146"
+    details: "https://filmbenia.vercel.app/movie/640146",
   },
   {
     title: "Guardians of the Galaxy Vol. 3",
     type: "Movie",
     image: "img/gotgv3.jpg",
-    realeaseDate: '5 May 2023',
+    realeaseDate: "5 May 2023",
     year: 2023,
-    details: "https://filmbenia.vercel.app/movie/447365"
+    details: "https://filmbenia.vercel.app/movie/447365",
   },
   {
     title: "Fantastic Four",
     type: "Movie",
     image: "img/fantastic4.jpg",
-    realeaseDate: '',
-    year: '',
-    details: "https://filmbenia.vercel.app/movie/617126"
+    realeaseDate: "",
+    year: "",
+    details: "https://filmbenia.vercel.app/movie/617126",
   },
   {
     title: "Moon Knight",
     type: "Series",
     image: "img/moon-knight.jpg",
-    realeaseDate: '',
+    realeaseDate: "",
     year: 2022,
-    details: "https://filmbenia.vercel.app/tv/92749"
+    details: "https://filmbenia.vercel.app/tv/92749",
   },
   {
     title: "The Guardians of the Galaxy Holiday Special",
     type: "Series",
     image: "img/gotg-holiday.jpg",
-    realeaseDate: '',
+    realeaseDate: "",
     year: 2022,
-    details: "https://www.imdb.com/title/tt13623136"
+    details: "https://www.imdb.com/title/tt13623136",
   },
   {
     title: "Armor Wars",
     type: "Series",
     image: "img/armor-wars.jpg",
-    realeaseDate: '',
-    year: '',
-    details: "https://filmbenia.vercel.app/tv/114470"
+    realeaseDate: "",
+    year: "",
+    details: "https://filmbenia.vercel.app/tv/114470",
   },
   {
     title: "Secret Invasion",
     type: "Series",
     image: "img/secret-invasion.jpg",
-    realeaseDate: '',
-    year: '',
-    details: "https://filmbenia.vercel.app/tv/114472"
+    realeaseDate: "",
+    year: "",
+    details: "https://filmbenia.vercel.app/tv/114472",
   },
   {
     title: "Iron Heart",
     type: "Series",
     image: "img/iron-heart.jpg",
-    realeaseDate: '',
-    year: '',
-    details: "https://www.imdb.com/title/tt13623126"
+    realeaseDate: "",
+    year: "",
+    details: "https://www.imdb.com/title/tt13623126",
   },
   {
     title: "Blade",
     type: "Movie",
     image: "img/blade.jpg",
-    realeaseDate: '',
-    year: '',
-    details: "https://filmbenia.vercel.app/movie/617127"
+    realeaseDate: "",
+    year: "",
+    details: "https://filmbenia.vercel.app/movie/617127",
   },
   {
     title: "I am Groot",
     type: "Series",
     image: "img/groot.jpg",
-    realeaseDate: '',
-    year: '',
-    details: "https://filmbenia.vercel.app/tv/114469"
+    realeaseDate: "",
+    year: "",
+    details: "https://filmbenia.vercel.app/tv/114469",
   },
 ]
 
--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-import movies from './data/movie.js'
-import { formatTime, join, toTimestamp, sortData } from './utils.js'
+import movies from "./data/movie.js"
+import { formatTime, join, toTimestamp, sortData } from "./utils.js"
 
-const row = document.querySelector('.row')
+const row = document.querySelector(".row")
 
-let a = [{ day: 'numeric' }, { month: 'short' }, { year: 'numeric' }];
-let s = join(new Date, a, ' ')
+// format UNIX epoch of current date to dd/MMM/yyyy
+let optionDate = [{ day: "numeric" }, { month: "short" }, { year: "numeric" }]
+const currDate = join(new Date(), optionDate, " ")
 
-const moviesSort = sortData(movies)
+// sort and filter movies
+const moviesSort = sortData(movies).filter(
+  (m) => toTimestamp(m.realeaseDate) >= toTimestamp(currDate) || m.realeaseDate === ""
+)
 
 // looping movieSort and send to dom
-moviesSort.forEach(movie => {
+moviesSort.forEach((movie) => {
   const { title, type, image, realeaseDate, year, details } = movie
 
   const rowContents = `
@@ -22,27 +26,34 @@ moviesSort.forEach(movie => {
           <h2 class="card-title">${title}</h2>
           <div class="card-texts">
             <p class="card-text">${realeaseDate ? realeaseDate : year}</p>
-            <p class="card-text type ${type === 'Series' ? 'series' : 'movie'}">${type}</p>
+            <p class="card-text type ${type === "Series" ? "series" : "movie"}">${type}</p>
           </div>
           <div class="countdown">
-            ${realeaseDate ? `
-              <div class="c-element">
-                <p class="big-text days" id="days">0</p>
-                <span>days</span>
-              </div>
-              <div class="c-element">
-                <p class="big-text hours" id="hours">0</p>
-                <span>hours</span>
-              </div>
-              <div class="c-element mins">
-                <p class="big-text minutes" id="mins">0</p>
-                <span>mins</span>
-              </div>
-              <div class="c-element">
-                <p class="big-text seconds" id="seconds">0</p>
-                <span>seconds</span>
-              </div>`
-      : `<p class="big-text hours">TBA ${year}</p>`}
+            ${
+              toTimestamp(realeaseDate) === toTimestamp(currDate)
+                ? `<p class="md-text">Showing today ${
+                    type === "Series" ? "on Disney+" : "in Cinemas"
+                  }</p>`
+                : realeaseDate
+                ? `
+                <div class="c-element">
+                  <p class="big-text days" id="days">0</p>
+                  <span>days</span>
+                </div>
+                <div class="c-element">
+                  <p class="big-text hours" id="hours">0</p>
+                  <span>hours</span>
+                </div>
+                <div class="c-element mins">
+                  <p class="big-text minutes" id="mins">0</p>
+                  <span>mins</span>
+                </div>
+                <div class="c-element">
+                  <p class="big-text seconds" id="seconds">0</p>
+                  <span>seconds</span>
+                </div>`
+                : `<p class="big-text">TBA ${year}</p>`
+            }
         </div>
       </div>
       <div class="card-actions">
@@ -51,8 +62,7 @@ moviesSort.forEach(movie => {
     </div>
   `
   // to dom
-  row.innerHTML += (toTimestamp(realeaseDate) > toTimestamp(s)) ?
-    rowContents : '' ? realeaseDate : rowContents
+  row.innerHTML += rowContents
 })
 
 const cards = document.getElementsByClassName("card")
@@ -60,40 +70,37 @@ const cards = document.getElementsByClassName("card")
 let countDownDate = []
 for (let i = 0; i < cards.length; i++) {
   countDownDate[i] = []
-  countDownDate[i]['el'] = cards[i]
-  countDownDate[i]['time'] = cards[i].getAttribute('data-date') ? new Date(cards[i].getAttribute('data-date')).getTime() : 0
-  countDownDate[i]['days'] = 0
-  countDownDate[i]['hours'] = 0
-  countDownDate[i]['seconds'] = 0
-  countDownDate[i]['minutes'] = 0
+  countDownDate[i]["el"] = cards[i]
+  countDownDate[i]["time"] = cards[i].getAttribute("data-date")
+    ? new Date(cards[i].getAttribute("data-date")).getTime()
+    : 0
+  countDownDate[i]["days"] = 0
+  countDownDate[i]["hours"] = 0
+  countDownDate[i]["seconds"] = 0
+  countDownDate[i]["minutes"] = 0
 
-  if (countDownDate[i]['time'] < "22 apr 2040") {
-    countDownDate[i]['el'].remove()
+  if (countDownDate[i]["time"] < "22 apr 2040") {
+    countDownDate[i]["el"].remove()
   }
 }
 
 setInterval(() => {
   for (let i = 0; i < countDownDate.length; i++) {
     const currentDate = new Date().getTime()
-    const distance = countDownDate[i]['time'] - currentDate
+    const distance = countDownDate[i]["time"] - currentDate
     const totalSeconds = distance / 1000
 
-    const daysEl = countDownDate[i]['el'].querySelector('.days')
-    const hoursEl = countDownDate[i]['el'].querySelector('.hours')
-    const minutesEl = countDownDate[i]['el'].querySelector('.minutes')
-    const secondsEl = countDownDate[i]['el'].querySelector('.seconds')
+    const daysEl = countDownDate[i]["el"].querySelector(".days")
+    const hoursEl = countDownDate[i]["el"].querySelector(".hours")
+    const minutesEl = countDownDate[i]["el"].querySelector(".minutes")
+    const secondsEl = countDownDate[i]["el"].querySelector(".seconds")
 
     const days = Math.floor(totalSeconds / 3600 / 24)
     const hours = Math.floor(totalSeconds / 3600) % 24
     const minutes = Math.floor(totalSeconds / 60) % 60
     const seconds = Math.floor(totalSeconds) % 60
 
-    if (distance < 0) {
-      // daysEl.innerHTML = 0
-      // hoursEl.innerHTML = 0
-      // minutesEl.innerHTML = 0
-      // secondsEl.innerHTML = 0
-    } else {
+    if (distance > 0) {
       daysEl.innerHTML = formatTime(days)
       hoursEl.innerHTML = formatTime(hours)
       minutesEl.innerHTML = formatTime(minutes)
@@ -101,5 +108,3 @@ setInterval(() => {
     }
   }
 }, 1000)
-
-
